faf7b8d5e33d387ebeb3b25792ba66069101f72d
Logan DeWilfond <logan.dewilfond@gmail.com>
2018-08-31 10:37:43 -0500

    commit

---
+++ FahrenheitToCelsius/FahrenheitToCelsius/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string nameString;
+
+	int degreesF;
+	int degreesC;
+	
+	cout << "Enter your temperature in fahrenheit >> ";
+	cin >> degreesF;
+
+	degreesC = (degreesF - 32) / 1.8;
+
+	cout << "Your temperature in Celsius is: " << degreesC;
+	
+
+	system("pause");
+	return 0;
+
+}
